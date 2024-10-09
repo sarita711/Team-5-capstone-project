460845c89a984e1c132d4ae7290f0deab2018f79
kasyap16 <2000040072ece@gmail.com>
2024-10-09 22:36:38 +0530

    m

--- cye/src/app/app-routing.module.ts
+++ cye/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashCompanyUserComponent } from './dash-company-user/dash-company-user.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dash-company-user', component: DashCompanyUserComponent},
+  {path: '', redirectTo: '/dash-company-user', pathMatch: 'full'},
+  {path: 'about-us', component: AboutUsComponent},
+  {path: '', redirectTo: '/about-us', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- cye/src/app/app.module.ts
+++ cye/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DashCompanyUserComponent } from './dash-company-user/dash-company-user.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashCompanyUserComponent,
+    FooterComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
