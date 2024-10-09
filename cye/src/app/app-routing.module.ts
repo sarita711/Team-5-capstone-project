737025db49d203a606e756aa7fd4ec06a380432a
shrestha.baruah <shrestha.baruah@siemens.com>
2024-10-09 17:02:36 +0530

    m

--- cye/src/app/app-routing.module.ts
+++ cye/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
-
+import { LoginComponent } from './login/login.component';
+import { EventPageComponent } from './event-page/event-page.component';
+ 
+const routes: Routes = [
+ 
+  { path: 'login', component: LoginComponent },
+  {path: 'event-page', component:EventPageComponent}
+];
+ 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- cye/src/app/app.module.ts
+++ cye/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EventPageComponent } from './event-page/event-page.component';
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EventPageComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
