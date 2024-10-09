6663e34aeef4c802295669539e5664cd24e14365
jhanat <jhanatchakma20@gmail.com>
2024-10-09 23:19:48 +0530

    normal-user

--- cye/src/app/app-routing.module.ts
+++ cye/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeNormalUserComponent } from './home-normal-user/home-normal-user.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'home-normal-user', component: HomeNormalUserComponent},
+  {path:'', redirectTo:'/home-normal-user', pathMatch: 'full'},
+  {path:'contact-us', component:ContactUsComponent },
+  {path:'', redirectTo: '/contact-us', pathMatch: 'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- cye/src/app/app.module.ts
+++ cye/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeNormalUserComponent } from './home-normal-user/home-normal-user.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeNormalUserComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
