15a36ae019e3ef45a051091840d293d8a2306cdd
sarita711 <sarita21042002@gmail.com>
2024-10-13 02:10:23 +0530

    events frontend connect and wishlist api frontend

--- cye/src/app/header/header.component.ts
+++ cye/src/app/header/header.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
+// header.component.ts
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  constructor(private router: Router) {}
+export class HeaderComponent implements OnInit {
+  userEmail: string | null = null;
+  userType: string | null = null;
 
-  logout() {
-    // Perform logout logic here (e.g., clear user session)
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    // Subscribe to changes in the current user
+    this.authService.currentUser.subscribe(user => {
+      if (user) {
+        this.userEmail = user.email;
+        this.userType = user.userType;
+      } else {
+        this.userEmail = null;
+        this.userType = null;
+      }
+    });
+  }
+
+  logout(): void {
+    this.authService.logout();
     this.router.navigate(['/welcome']);
   }
 }
